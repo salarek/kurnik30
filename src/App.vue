1cdd47cfe6c6f1aa5acf66e624235489db9d18b2
salarek <salarek@pop-os.localdomain>
2020-10-21 10:34:27 +0200

    pierwsza gra

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,30 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <input v-model="gameName" />
+    <div v-if="gameName === 'saper'">
+      <saper />
+    </div>
+    <div v-if="gameName === 'helloWorld'">
+      <HelloWorld msg="Welcome to Your Vue.js App" />
+    </div>
+    <!-- <button @click="chooseGame">Wybierz</button> -->
   </div>
 </template>
 
 <script>
 import HelloWorld from "./components/HelloWorld.vue";
+import saper from "./components/saper.vue";
 
 export default {
   name: "App",
   components: {
     HelloWorld,
+    saper,
+  },
+  data() {
+    return {
+      gameName: "",
+    };
   },
 };
 </script>
