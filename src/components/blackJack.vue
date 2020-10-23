c51a615037cc44865cf5a4a365fb396ec93ab41c
Lukasz Malinowski <lukaszmalinowski@pop-os.localdomain>
2020-10-23 15:22:41 +0200

    black  jack struktura

--- src/components/blackJack.vue
+++ src/components/blackJack.vue
@@ -1,6 +1,6 @@
 <template>
   <v-app id="inspire">
-    <!-- <v-navigation-drawer v-model="drawer" app clipped>
+    <v-navigation-drawer v-model="drawer" app clipped>
       <v-list dense>
         <v-list-item link>
           <v-list-item-action>
@@ -19,15 +19,39 @@
           </v-list-item-content>
         </v-list-item>
       </v-list>
-    </v-navigation-drawer> -->
+    </v-navigation-drawer>
 
     <v-app-bar app clipped-left>
-      <!-- <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon> -->
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
       <v-toolbar-title>Kurnik 3.0</v-toolbar-title>
     </v-app-bar>
 
     <!--tutaj umieszczam to co jest widoczne w gierce  -->
-    <v-main> </v-main>
+    <v-main>
+      <div class="blackJack">
+        <div class="mainPage">
+          <h1>Black Jack</h1>
+        </div>
+        <div class="Krupier">
+          <div class="karty">
+            <img src="./PNG/gray_back.png" alt="" />
+            <img src="./PNG/gray_back.png" alt="" />
+          </div>
+          <h2>Suma punktów Krupiera: {{ krupierPoints }}</h2>
+        </div>
+        <div class="player">
+          <h2>Suma punktów Gracza: {{ playerPoints }}</h2>
+          <div class="karty">
+            <img src="./PNG/KC.png" alt="" />
+            <img src="./PNG/AH.png" alt="" />
+          </div>
+        </div>
+        <div class="gamePanel">
+          <button>Draw</button>
+          <button>Check</button>
+        </div>
+      </div>
+    </v-main>
 
     <v-footer app>
       <span>&copy; {{ new Date().getFullYear() }}</span>
@@ -41,10 +65,20 @@ export default {
   props: {
     source: String,
   },
-  data: function() {
+  data: function () {
     return {
-      symbols: ["pik", "kier", "tref", "karo"],
-      cardValue: [
+      playerPoints: 19,
+      krupierPoints: 15,
+      deck: [],
+    };
+  },
+  created: {
+    create: "createDeck() ",
+  },
+  methods: {
+    createDeck: function () {
+      const symbols = ["C", "D", "H", "S"];
+      const mark = [
         "2",
         "3",
         "4",
@@ -58,37 +92,19 @@ export default {
         "Q",
         "K",
         "A",
-      ],
-      deck: new Array(),
-    };
-  },
-  created: {
-    createdeck: "createDeck",
-  },
-  methods: {
-    createDeck: function() {
-      for (let i = 0; i < this.cardValue.length; i++) {
-        for (let j = 0; j < this.symbols.length; j++) {
-          var value = parseInt(this.cardValue[i]);
-          if (
-            this.cardValue[i] == "J" ||
-            this.cardValue[i] == "Q" ||
-            this.cardValue[i] == "K"
-          ) {
-            value = 10;
-          } else if (this.cardValue[i] == "A") {
-            value = 11;
-          }
-          console.log(value);
-          var card = {
-            Symbol: this.symbol[i],
-            CardValue: this.CardValue[i],
-            Value: value,
-          };
-          this.deck.push(card);
-        }
-      }
+      ];
+      var pool = [];
+      mark.map((m) => {
+        symbols.map((s) => {
+          pool.push(`${m}${s}`);
+        });
+      });
+      console.log(pool);
+      pool = this.deck;
     },
+    drawCard() {},
+    countPoints() {},
+    check() {},
   },
 };
 </script>
@@ -122,5 +138,37 @@ export default {
 .con {
   width: 800px;
   height: 800px;
+}
+.mainPage {
+  text-align: center;
+  height: 10%;
+}
+.karty {
+  display: flex;
+  justify-content: center;
+}
+img {
+  width: 10%;
+}
+button {
+  background-color: #4caccc; /* Green */
+  border: none;
+  color: white;
+  padding: 15px 32px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+}
+.gamePanel {
+  display: flex;
+  justify-content: space-around;
+  width: 20%;
+  margin-left: 40%;
+  margin-top: 3%;
+}
+.blackJack {
+  background-color: green;
+  height: 100%;
 }
 </style>
