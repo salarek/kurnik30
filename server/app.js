3d92e0be5d7cc217f1b308104ea58f691c89e17f
salarek <salarek@o2.pl>
2020-10-27 15:08:28 +0100

    punktacja done

--- server/app.js
+++ server/app.js
@@ -1,10 +1,12 @@
 const Express = require("express");
+const { Socket } = require("socket.io-client");
 const Http = require("http").Server(Express);
 const Socketio = require("socket.io")(Http);
 const {
   joinUser,
   // getCurrentUser,
-  // getRoomUsers,
+  addPointsToUser,
+   getRoomUsers,
   userLeave,
   getNextUser,
   // getNextSocketUser,
@@ -14,8 +16,11 @@ Socketio.on("connection", (socket) => {
   socket.on("userInfo", (user) => {
     console.log(user);
     joinUser(socket.id, user);
+    let users = getRoomUsers();
+    console.log(users)
+    Socketio.emit("allUsers", users);
   });
-  socket.emit("msg", "dupa");
+ 
   socket.on("plansza", (board) => {
     socket.broadcast.emit("planszaBroadcast", board);
   });
@@ -24,10 +29,26 @@ Socketio.on("connection", (socket) => {
     console.log(drawer);
     Socketio.emit("queUser", drawer);
   });
+  socket.on("gameOver", (gameOver) => {
+   
+    Socketio.emit("gameOverRec", gameOver);
+  });
+
   socket.on("msg", (msg) => {
     console.log(msg);
     Socketio.emit("recmsg", msg);
   });
+  socket.on("sendSettings", (boardWidth, difficulty, startGame)=>{
+    console.log(difficulty);
+    socket.broadcast.emit("settingsRec", boardWidth, difficulty, startGame);
+  });
+  socket.on("sendPoints", (points, username)=>{
+    addPointsToUser(points, username);
+    let users = getRoomUsers();
+    console.log(users)
+    Socketio.emit("allUsers", users,)
+  })
+
   socket.on("disconnect", () => {
     userLeave(socket.id);
   });
